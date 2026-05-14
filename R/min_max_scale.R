afea6cc28cad9f1cac6aa686cc44968a4b601261
daisyabcxyz <nguyenng@lawrence.edu>
2026-05-14 17:10:35 +0000

    added documentation

--- R/min_max_scale.R
+++ R/min_max_scale.R
@@ -1,3 +1,12 @@
+#' Find the difference to the minimum value, with respect to the min-max difference
+#'
+#' @param x A list of numbers
+#'
+#' @returns A scale with respect to the the min-max difference
+#' @export
+#'
+#' @examples
+#' min_max_scale(1:3)
 min_max_scale <- function(x)
 {
   y <- (x - min(x, na.rm = TRUE))/(max(x, na.rm = TRUE)-min(x, na.rm = TRUE))
--- R/theme_academic.R
+++ R/theme_academic.R
@@ -1,3 +1,16 @@
+#' Adjust the layout of a plot
+#'
+#' @param base_size size of the plot
+#'
+#' @returns a plot with the input base size, a bold plot title in the center and no grid
+#' @importFrom ggplot2 theme_classic theme element_blank element_text
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' p<- ggplot(data = mtcars) + geom_point(aes(x= mpg, y= hp)) + labs(title = "plot_title")
+#' p
+#' p + theme_academic()
 theme_academic <- function(base_size =12){
   ggplot2::theme_classic(base_size= base_size) +
   ggplot2::theme(plot.title = ggplot2::element_text(face="bold",hjust =0.5),
